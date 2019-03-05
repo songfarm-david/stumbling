d6e61a43f6a0a6512511d9a2704329815357af77
David Gaskin <davidburkegaskin@gmail.com>
2019-03-05 11:56:53 -0800

    another test

--- src/components/subscribe-form.js
+++ src/components/subscribe-form.js
@@ -1,22 +1,21 @@
 import React from "react"
 
 const SubscribeForm = (props) => (
-	<form name="contact" action="/" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+	<form name="contact" method="post" data-netlify="true">
 
         <div className="field">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" required/>
+            <input type="text" name="name" id="name" value="dave"/>
         </div>
         <div className="field">
             <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" required/>
+            <input type="text" name="email" id="email" value="email@email.com" />
         </div>
         <div className="field">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="6" required></textarea>
+            <textarea name="message" id="message" rows="6"></textarea>
         </div>
 
-		  <input type="hidden" name="bot-field" />
 		  <input type="submit" value="Send Message" className="" />
 
     </form>
