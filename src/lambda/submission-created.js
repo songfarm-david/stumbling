2cef4609356a86c34d34aadae9a127ad94087be0
David Gaskin <davidburkegaskin@gmail.com>
2019-03-12 01:42:16 -0700

    update submission created function

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,7 +1,8 @@
 exports.handler = function(event, context, callback) {
 	// event is submission-created
+	console.log('hello world');
 	console.log(event, context)
 
 	const { payload, site } = context
-	console.log(payload, site);
+	console.log(payload, site)
 }
