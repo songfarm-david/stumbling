7743ae30ed51aadbb6960f36c17c79c5f6562656
David Gaskin <davidburkegaskin@gmail.com>
2019-03-12 01:41:21 -0700

    update subscribe form ui; add netlify-lambda to package.json

--- src/components/subscribe-form.js
+++ src/components/subscribe-form.js
@@ -1,28 +1,28 @@
 import React from "react"
 
 const SubscribeForm = (props) => (
+	<>
+	<h3>Join the mailing list to get notified of new posts.</h3>
+
 	<form name="contact" action="/" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
 
-		<input type="hidden" name="form-name" value="contact" />
-		{/* spam filtering hidden input field */}
+		{/* spam filtering hidden input field - required to get the form to work with netlify */}
 		<input type="hidden" name="bot-field" />
+		<input type="hidden" name="form-name" value="contact" />
 
-		<div className="field">
+		<div>
 			<label htmlFor="name">Name</label>
 			<input type="text" name="name" id="name" required />
 		</div>
-        <div className="field">
+        <div>
             <label htmlFor="email">Email</label>
             <input type="text" name="email" id="email" required />
         </div>
-        <div className="field">
-            <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="6"></textarea>
-        </div>
 
-		  <input type="submit" value="Send Message" />
+		  <input type="submit" value="Subscribe" />
 
     </form>
+	 </>
 )
 
 export default SubscribeForm
