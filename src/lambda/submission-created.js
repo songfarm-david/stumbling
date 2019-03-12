fe89a1c51c3ba73a575c8bbcc467a614f8c325e4
David Gaskin <davidburkegaskin@gmail.com>
2019-03-12 02:26:49 -0700

    update lambda function

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,8 +1,19 @@
+const statusCode = 200
+const headers = {
+  "Access-Control-Allow-Origin" : "*",
+  "Access-Control-Allow-Headers": "Content-Type"
+}
+
 exports.handler = function(event, context, callback) {
 	// event is submission-created
 	console.log('hello world')
-	console.log(event, context)
-
-	const { payload, site } = context
-	console.log(payload, site)
+	// console.log(event, context)
+	//
+	// const { payload, site } = context
+	// console.log(payload, site)
+	callback(null, {
+      statusCode,
+      headers,
+      body: 'Let there be light!'
+    });
 }
