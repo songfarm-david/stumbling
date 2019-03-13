3967174332898331e10259b5ecc1610d8a48cd21
David Gaskin <davidburkegaskin@gmail.com>
2019-03-13 12:45:41 -0700

    update submission-created function

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -5,16 +5,22 @@ const headers = {
 }
 
 exports.handler = function(event, context, callback) {
+
+   var body = JSON.parse(event.body);
+   console.log('this is my body');
+   console.log(body);
 	// event is submission-created
-   const body = JSON.parse(event.body).payload
-	console.log(JSON.parse(event.body).payload)
+   // const body = JSON.parse(event.body).payload
+	// console.log(JSON.parse(event.body).payload)
 	// console.log(event, context)
 	//
 	// const { payload, site } = context
 	// console.log(payload, site)
+
 	callback(null, {
       statusCode,
       headers,
-      body: body
+      body: "Joy to the world!"
     });
+
 }
