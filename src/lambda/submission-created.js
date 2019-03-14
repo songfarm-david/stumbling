3773516257951dbf64a7df58be1b056c8dddf3e7
David Gaskin <davidburkegaskin@gmail.com>
2019-03-14 03:20:58 -0700

    update function

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -7,24 +7,25 @@ const headers = {
 exports.handler = function(event, context, callback) {
 
    // var body = JSON.parse(event.body);
-   console.log("Smush");
-   console.log(event.path);
-   console.log(event.httpMethod);
-   console.log(event.queryStringParameters);
+   console.log('path', event.path);
+   console.log('method', event.httpMethod);
+   console.log('queryStringParameters', event.queryStringParameters);
    console.log(JSON.parse(event.body).payload);
 
-	// event is submission-created
-   // const body = JSON.parse(event.body).payload
-	// console.log(JSON.parse(event.body).payload)
-	// console.log(event, context)
-	//
-	// const { payload, site } = context
-	// console.log(payload, site)
+   let body = JSON.parse(event.body).payload
+   let formName = body.data.form_name
+   let name, comment
 
-	// callback(null, {
-   //    statusCode,
-   //    headers,
-   //    body: "Joy to the world!"
-   //  });
+   if (formName === 'subscription-form') {
+      callback(null, {
+         body: "Joy to the world!"
+      });
+      return console.log('Returning because this is for the subscription form.');
+   } else {
+      name = body.data.name
+      comment = body.data.comment
+   }
+
+
 
 }
