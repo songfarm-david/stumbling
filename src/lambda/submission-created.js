fd23ccfc351a1cb65522bb0c4bfc03de3ec3fde2
David Gaskin <davidburkegaskin@gmail.com>
2019-03-18 15:26:18 -0700

    change param to qs

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -51,7 +51,7 @@ exports.handler = async (event, context, callback) => {
          // postComment(url, comment);
 
          // NOTE: Comment API: https://developer.wordpress.org/rest-api/reference/comments/#create-a-comment
-         request.post({ url, body: JSON.stringify(comment), json: true },
+         request.post({ url, qs: JSON.stringify(comment) },
             function callback(err, httpResponse, body) {
                console.log('anything')
                // if (err) {
