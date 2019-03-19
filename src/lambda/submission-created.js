56dce344b4dec8aba6eceb5cd0f3e9c7914994c5
David Gaskin <davidburkegaskin@gmail.com>
2019-03-18 23:15:36 -0700

    what's going on?

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,4 +1,4 @@
-// var WPAPI = require( 'wpapi' );
+var WPAPI = require( 'wpapi' );
 
 exports.handler = async (event, context, callback) => {
 
@@ -8,26 +8,32 @@ exports.handler = async (event, context, callback) => {
    // TODO: comment-form here and in form actions should be put in env var
    if (body.form_name == 'comment-form') {
 
-      let author_name = encodeURI(body.data.name)
-      let author_comment = encodeURI(body.data.comment)
-      let id = encodeURI(body.data.postId)
+      var wp = new WPAPI({
+         endpoint: 'https://stumblingtowardsenlightenment.com/wp-json',
+         username: 'bobo',
+         password: 'mr?8(+JSx7z4'
+      });
+
+      // let author_name = encodeURI(body.data.name)
+      // let author_comment = encodeURI(body.data.comment)
+      // let id = encodeURI(body.data.postId)
 
       // TODO: encode the URL here
-      console.log('vars:', author_name, author_comment, id);
+      // console.log('vars:', author_name, author_comment, id);
 
-      // var wp = new WPAPI({
-      //    endpoint: 'https://stumblingtowardsenlightenment.com/wp-json',
-      //    username: 'bobo',
-      //    password: 'mr?8(+JSx7z4'
-      // });
-      //
-      // wp.comments().create({
-      //    author_name: bra,
-      //    content: bro,
-      //    post: id
-      // }).then(function( response ) {
-      //    console.log( response );
-      // });
+      let comment = {
+         author_name: encodeURI(author_name),
+         author_comment: encodeURI(author_name),
+         post: body.data.postId
+      }
+
+      wp.comments().create({
+         author_name: bra,
+         content: bro,
+         post: id
+      }, function(args) { console.log(args) }).then(function( response ) {
+         console.log( response );
+      });
 
    }
 
