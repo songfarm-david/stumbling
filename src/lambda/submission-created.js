262fe6f5456afdea73c30f1157b20f92d6b18e1a
David Gaskin <davidburkegaskin@gmail.com>
2019-03-18 21:55:14 -0700

    cleaned up function

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,96 +1,34 @@
-// var request = require("request");
-// var urlencode = require('urlencode');
 var WP_API = require( 'wpapi' );
-
-// console.log(request.post);
-// function postComment() {
-//    console.log('postComment called');
-// }
 
 exports.handler = async (event, context, callback) => {
 
-   // try {
-   //
-   //    console.log('About to "TRY"');
-   //
-   //    // Do not handle requests if the request
-   //    // type is something other than `POST` or
-   //    // if the request body is empty.
-   //    if (event.httpMethod !== 'POST' || !event.body) {
-   //       callback(null, {
-   //         statusCode: 400,
-   //         body: JSON.stringify({ status: 'Bad Request' }),
-   //       });
-   //       return;
-   //    }
-   //
-   //    console.log('Made it past the condition!');
+   let body = JSON.parse(event.body).payload;
+   console.log('body:', body);
 
-      let body = JSON.parse(event.body).payload;
-      // console.log(body);
-      console.log('body:', body);
+   // TODO: comment-form here and in form actions should be put in env var
+   if (body.form_name == 'comment-form') {
 
-      // if from comment form
-      // TODO: comment-form here and in form actions should be put in env var
-      if (body.form_name == 'comment-form') {
-         // console.log('Submission passed as a comment-form comment');
+      let bra = encodeURI(body.data.name)
+      let bro = encodeURI(body.data.comment)
+      let id = encodeURI(body.data.postId)
 
-         // let url = process.env.WP_COMMENTS + "?";
-         // console.log('url to send:', url);
+      // TODO: encode the URL here
+      console.log('vars:', author_name, author_comment, id);
 
+      var wp = new WP_API({
+         endpoint: 'https://stumblingtowardsenlightenment.com/wp-json',
+         username: 'bobo',
+         password: 'mr?8(+JSx7z4'
+      });
 
-         // let comment = {
-         //    "author_name": body.data.name,
-         //    "content": body.data.comment,
-         //    "date": body.created_at,
-         //    "post": body.data.postId,
-         //    "url": url + '/' + body.data.slug,
-         //    "meta": {
-         //       "slug": body.data.slug
-         //    }
-         // };
+      wp.comments().create({
+         author_name: bra,
+         content: bro,
+         post: id
+      }).then(function( response ) {
+         console.log( response );
+      });
 
-         let author_name = encodeURI(body.data.name)
-         let author_comment = encodeURI(body.data.comment)
-         let id = encodeURI(body.data.postId)
-
-         // TODO: encode the URL here
-         // let commentStr = `author_name=${author_name}&content=${author_comment}&post=${id}`
-         console.log('vars:', author_name, author_comment, id);
-
-         var wp = new WP_API({
-            endpoint: 'https://stumblingtowardsenlightenment.com/wp-json',
-            username: 'bobo',
-            password: 'mr?8(+JSx7z4'
-         });
-
-         wp.comments().create({
-            author_name: author_name,
-            content: author_comment,
-            post: id
-         }).then(function( response ) {
-            console.log( response );
-         });
-
-
-         // request.post({ url, body: commentStr },
-         //    function callback(err, httpResponse, body) {
-         //       console.log('anything')
-         //       // if (err) {
-         //       //    return console.error('upload failed:', err)
-         //       // }
-         //       console.log('hello?', err, httpResponse, body)
-         //       return console.log('hello?', err, httpResponse, body)
-         //    }
-         // );
-
-      }
-
-   // } catch (error) {
-   //    callback(null, {
-   //    statusCode: 500,
-   //    body: JSON.stringify({ status: 'error' }),
-   //  });
-   // }
+   }
 
 }
