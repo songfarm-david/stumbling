374c90ca04d64fff6cce50020c35be20a86d2dfe
David Gaskin <davidburkegaskin@gmail.com>
2019-06-02 14:34:13 -0700

    update testFunc

--- src/lambda/testFunc.js
+++ src/lambda/testFunc.js
@@ -1,6 +1,8 @@
 exports.handler = async (event, context, callback) => {
-	const body = JSON.parse(event.body).payload
-	callback(null, {
+	const body = JSON.parse(event.body).payload;
+
+	console.log(body);
+	return callback(null, {
     statusCode: 200,
     body: body
     });
