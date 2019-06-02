a43ddf4f9c5ab0c0ac0fe3a38677f7dd054e9db5
David Gaskin <davidburkegaskin@gmail.com>
2019-06-02 14:59:59 -0700

    add more debug code to testFunc

--- src/lambda/testFunc.js
+++ src/lambda/testFunc.js
@@ -10,11 +10,11 @@ exports.handler = async (event, context, callback) => {
 
 	console.log(event, context, callback);
 
-	// const body = JSON.parse(event.body).payload;
+	const body = JSON.parse(event.body).payload;
 
-	// console.log(body);
+	console.log(body);
 	return callback(null, {
 		statusCode: 200,
-		body: "Hey ya"
+		body: body
 	});
 }
