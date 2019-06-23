52f2ce721082bbcda9ca539cfae195543a8f298c
David Gaskin <davidburkegaskin@gmail.com>
2019-06-23 14:52:53 -0700

    create env files; update git ignore; test mailchimp api call

--- src/lambda/mailchimp.js
+++ src/lambda/mailchimp.js
@@ -1,44 +1,53 @@
-/*
- * Handles MailChimp new subscriber creation
- *
- */
+const axios = require('axios')
+// require("dotenv").config()
 
-/**
- * Audience ID: 684872185b
- */
+const {
+  MAILCHIMP_USERNAME,
+  MAILCHIMP_API_KEY,
+  MAILCHIMP_DATA_NO,
+  MAILCHIMP_LIST_ID
+} = process.env
 
- /*
-  * Resources:
-  *
-  * Troubleshooting: https://www.raymondcamden.com/2019/01/15/customized-form-handling-on-netlify-with-serverless-functions
-  * MailChimp API Resource: https://developer.mailchimp.com/documentation/mailchimp/guides/get-started-with-mailchimp-api-3/
-  * Managing Contacts with Mailchimp: https://developer.mailchimp.com/documentation/mailchimp/guides/manage-subscribers-with-the-mailchimp-api/
-  * List Schema: https://api.mailchimp.com/schema/3.0/Lists/Members/Instance.json?_ga=2.12525744.504634568.1559680393-1547731669.1550175669
-  * API Reference for Lists: https://developer.mailchimp.com/documentation/mailchimp/reference/lists/#create-post_lists_list_id
-  */
+const TestAPIURL = 'https://us20.api.mailchimp.com/3.0/'
+const Credentials = MAILCHIMP_USERNAME+':'+ MAILCHIMP_API_KEY
 
-/*
- * curl --request GET \
-	--url 'https://<dc>.api.mailchimp.com/3.0/' \
-	--user 'anystring:<your_apikey>'
- * // curl --request GET \ --url 'https://us20.api.mailchimp.com/3.0/' \ --user 'steBobo:4569d5b1e159dd9e1697142499bd6d1f-us20'
- * This one works: curl -X GET 'https://us20.api.mailchimp.com/3.0/' -u 'steBobo:4569d5b1e159dd9e1697142499bd6d1f-us20'
- */
+console.log(TestAPIURL\nCredentials);
 
-// const APIRoot = "https://us20.api.mailchimp.com/3.0"
-// const User    = "steBobo"
-const APIKey  = process.env.MAILCHIMP_API_KEY
-const ListID  = process.env.MAILCHIMP_LIST_ID // (List/Audience ID)
-
-exports.handler = async (event, context, callback) => {
-
-	console.log(event, context, callback);
-	const body = JSON.parse(event.body).payload;
-	console.log(body);
-
-	// return callback(null, {
-	// 	statusCode: 200,
-	// 	body: body
-	// });
-
+let testRequest = {
+   "email_address": "urist.mcvankab@freddiesjokes.com",
+   "status": "subscribed",
+   "merge_fields": {
+      "FNAME": "Donnie",
+      "LNAME": "Brasco"
+   }
 }
+
+exports.handler = (event, context, callback) => {
+	axios({
+		method: 'post',
+		url: TestAPIURL,
+		data: testRequest,
+		headers: {
+			'Authorization': 'Basic ' + Credentials
+		}
+	})
+	.then(res => {
+		console.log(res);
+	})
+	.catch(err => {
+		console.log(err);
+	})
+	.finally(() => {
+		console.log('Finally. Hello, world');
+	})
+}
+
+// Want to use async/await? Add the `async` keyword to your outer function/method.
+// async function getUser() {
+//   try {
+//     const response = await axios.get('/user?ID=12345');
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,157 +1,174 @@
-// import postData from './fetch-url'
-
-// NOTE: article with good example fetch request: https://www.netlify.com/blog/2018/03/29/jamstack-architecture-on-netlify-how-identity-and-functions-work-together/
-
-const fetch = require("node-fetch");
-
-const TestAPIURL = 'https://us20.api.mailchimp.com/3.0/lists/684872185b/members/'
-
-const {
-  MAILCHIMP_API_KEY,
-  MAILCHIMP_DATA_NO,
-  MAILCHIMP_LIST_ID,
-  MAILCHIMP_USERNAME
-} = process.env
-
-let testRequest = {
-   "email_address": "urist.mcvankab@freddiesjokes.com",
-   "status": "subscribed",
-   "merge_fields": {
-      "FNAME": "Donnie",
-      "LNAME": "Brasco"
-   }
-}
-
-// form submission event triggered from Netlify
-exports.handler = (event, context, callback) => {
-
-   console.log('Hello, Dave. You are fun. You are confident.')
-   let payload = JSON.parse(event.body).payload
-   console.log('payload:', payload);
-   // console.log('httpMethod: ', event.httpMethod);
-   // const MailchimpAPIUrl = 'https://' + MAILCHIMP_DATA_NO + '.api.mailchimp.com/3.0/lists/' + MAILCHIMP_LIST_ID + '/members/'
-   // const Username = MAILCHIMP_USERNAME + ':' + MAILCHIMP_API_KEY
-   // console.log(TestAPIURL, testRequest);
-   try {
-      console.log('about to try');
-      fetch(TestAPIURL)
-      .then(res => {
-         console.log('what is res?', res);
-         if (payload.form_name == 'subscription-form') {
-            console.log('form name was subscription');
-         } else if (payload.form_name == 'comment-form') {
-            console.log('form name was a comment');
-            // url = 'https://stumblingtowardsenlightenment.com/wp-json/wp/v2/comments' // /comments
-            // data = {
-            //    postId: payload.data.postId,
-            //    name: payload.data.name,
-            //    comment: payload.data.comment
-            // }
-            // console.log(url, data);
-         }
-         // callback(null, {
-         //    statusCode: 200,
-         //    body: JSON.stringify(res.data)
-         // })
-         return res
-      })
-      .catch(e => {
-         console.log('in the fetch catch', e);
-         callback(e);
-      })
-   } catch (e) {
-      console.log('in the catch of the try', e);
-      callback(e, { statusCode: 500, body: "Internal Server Errors: " + e })
-   }
-
-}
-
-// try {
-
-//    console.log('say try?', payload)
-   // if (payload.form_name == 'subscription-form') {
-   //    // set url (use ENV VAR)
-   //    url = 'mailchimp endpoint'
-   //    data = {
-   //       // ...
-   //    }
-   // }
-
-   // if (payload.form_name == 'comment-form') {
-   //    console.log('this is a comment');
-   //    url = 'https://stumblingtowardsenlightenment.com/wp-json/wp/v2/comments' // /comments
-   //    data = {
-   //       postId: payload.data.postId,
-   //       name: payload.data.name,
-   //       comment: payload.data.comment
-   //    }
-   //    console.log(url, data);
-   // }
-
-   // fetch('urlToApi')
-   // .then(response => {
-   //    console.log("Did we get a response? ", response)
-   //    return response.json()
-   // })
-   // .then(myJson => {
-   //    console.log(JSON.stringify(myJson))
-   // })
-   // .catch(error => {
-   //    console.log("error: ", error);
-   //    throw new Error('Something bad happened.', error)
-   // })
-
-// } catch (e) {
-//    callback(null, {
-//       statusCode: 500,
-//       body: "Internal Server Error: " + e
-//    });
+//
+// const TestAPIURL = 'https://us20.api.mailchimp.com/3.0/lists/684872185b/members/'
+// // NOTE: these will be environmental variables
+//
+// const {
+//   MAILCHIMP_API_KEY,
+//   MAILCHIMP_DATA_NO,
+//   MAILCHIMP_LIST_ID,
+//   MAILCHIMP_USERNAME
+// } = process.env
+//
+// let testRequest = {
+//    "email_address": "urist.mcvankab@freddiesjokes.com",
+//    "status": "subscribed",
+//    "merge_fields": {
+//       "FNAME": "Donnie",
+//       "LNAME": "Brasco"
+//    }
 // }
-
-// the event object:
-// {
-//  "path": "Path parameter",
-//  "httpMethod": "Incoming request's method name"
-//  "headers": {Incoming request headers}
-//  "queryStringParameters": {query string parameters }
-//  "body": "A JSON string of the request payload."
-//  "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
-// }
-
-// var apiPromise = WPAPI.discover( 'https://stumblingtowardsenlightenment.com' )
-// apiPromise.then(function(site) {
-//    site.comments().create(comment, function(err, data) {
-//       console.log(err, data)
-//    }).then(function( response ) {
-//       console.log( response );
-//    }).catch(function( err ) {
-//       console.log(err);
-//    });
-// })
-
-// wp.comments().create(comment, function(err, data) {
-//    console.log(err, data)
-// }).then(function( response ) {
-//    console.log( response );
-// }).catch(function( err ) {
-//    console.log(err);
-// });
-
-// let body
-// if (event.body.payload !== '') {
-//    // body = JSON.parse(event.body).payload;
-//    console.log('body:', body);
-//    callback(null, {
-//       statusCode: 200,
-//       body: JSON.stringify(event.body).payload
+//
+// // form submission event triggered from Netlify
+// exports.handler = (event, context, callback) => {
+//
+//    console.log('submission created callback fired');
+//
+//    // fetch API URL for MailChimp
+//    fetch(TestAPIURL, {
+//       // config credentials here
+//       method: 'POST',
+//       headers: new Headers({
+//          'Authorization': Credentials
+//       }),
+//       credentials: 'include'
+//    }).
+//    then((res) => {
+//       console.log('fetch succeeded', res);
+//    }).
+//    catch(err => {
+//       console.log('fetch failed', err);
 //    })
+//
+// //
+//    // let payload = JSON.parse(event.body).payload
+//    // console.log('payload:', payload)
+//    // console.log(TestAPIURL, testRequest)
+//
+//    // fetch(TestAPIURL, {
+//    //    method: 'POST',
+//    //    headers: new Headers({
+//    //       'Authorization':
+//    //    }),
+//    //    // credentials: 'include'
+//    // })
+//    // .then(res => {
+//    //       console.log('what is res?', res)
+//    //       return JSON.stringify(res)
+//    //       // if (payload.form_name == 'subscription-form') {
+//    //          // console.log('form name was subscription');
+//    //       // } else if (payload.form_name == 'comment-form') {
+//    //          // console.log('form name was a comment');
+//    //          // url = 'https://stumblingtowardsenlightenment.com/wp-json/wp/v2/comments' // /comments
+//    //          // data = {
+//    //          //    postId: payload.data.postId,
+//    //          //    name: payload.data.name,
+//    //          //    comment: payload.data.comment
+//    //          // }
+//    //          // console.log(url, data);
+//    //       // }
+//    //       // callback(null, {
+//    //       //    statusCode: 200,
+//    //       //    body: JSON.stringify(res.data)
+//    //       // })
+//    //    })
+//    //    .catch(e => {
+//    //       console.log('in the fetch catch', e);
+//    //       callback(e);
+//    //    })
+//
 // }
-
-// TODO: comment-form here and in form actions should be put in env var
-// if (body && body.form_name == 'comment-form') {
-
-   // var wp = new WPAPI({
-   //    endpoint: 'https://stumblingtowardsenlightenment.com/wp-json',
-   //    username: 'bobo',
-   //    password: 'mr?8(+JSx7z4'
-   // });
+//
+//
+// // console.log('httpMethod: ', event.httpMethod);
+// // const MailchimpAPIUrl = 'https://' + MAILCHIMP_DATA_NO + '.api.mailchimp.com/3.0/lists/' + MAILCHIMP_LIST_ID + '/members/'
+// // const Username = MAILCHIMP_USERNAME + ':' + MAILCHIMP_API_KEY
+//
+// // try {
+//
+// //    console.log('say try?', payload)
+//    // if (payload.form_name == 'subscription-form') {
+//    //    // set url (use ENV VAR)
+//    //    url = 'mailchimp endpoint'
+//    //    data = {
+//    //       // ...
+//    //    }
+//    // }
+//
+//    // if (payload.form_name == 'comment-form') {
+//    //    console.log('this is a comment');
+//    //    url = 'https://stumblingtowardsenlightenment.com/wp-json/wp/v2/comments' // /comments
+//    //    data = {
+//    //       postId: payload.data.postId,
+//    //       name: payload.data.name,
+//    //       comment: payload.data.comment
+//    //    }
+//    //    console.log(url, data);
+//    // }
+//
+//    // fetch('urlToApi')
+//    // .then(response => {
+//    //    console.log("Did we get a response? ", response)
+//    //    return response.json()
+//    // })
+//    // .then(myJson => {
+//    //    console.log(JSON.stringify(myJson))
+//    // })
+//    // .catch(error => {
+//    //    console.log("error: ", error);
+//    //    throw new Error('Something bad happened.', error)
+//    // })
+//
+// // } catch (e) {
+// //    callback(null, {
+// //       statusCode: 500,
+// //       body: "Internal Server Error: " + e
+// //    });
+// // }
+//
+// // the event object:
+// // {
+// //  "path": "Path parameter",
+// //  "httpMethod": "Incoming request's method name"
+// //  "headers": {Incoming request headers}
+// //  "queryStringParameters": {query string parameters }
+// //  "body": "A JSON string of the request payload."
+// //  "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
+// // }
+//
+// // var apiPromise = WPAPI.discover( 'https://stumblingtowardsenlightenment.com' )
+// // apiPromise.then(function(site) {
+// //    site.comments().create(comment, function(err, data) {
+// //       console.log(err, data)
+// //    }).then(function( response ) {
+// //       console.log( response );
+// //    }).catch(function( err ) {
+// //       console.log(err);
+// //    });
+// // })
+//
+// // wp.comments().create(comment, function(err, data) {
+// //    console.log(err, data)
+// // }).then(function( response ) {
+// //    console.log( response );
+// // }).catch(function( err ) {
+// //    console.log(err);
+// // });
+//
+// // let body
+// // if (event.body.payload !== '') {
+// //    // body = JSON.parse(event.body).payload;
+// //    console.log('body:', body);
+// //    callback(null, {
+// //       statusCode: 200,
+// //       body: JSON.stringify(event.body).payload
+// //    })
+// // }
+//
+// // TODO: comment-form here and in form actions should be put in env var
+// // if (body && body.form_name == 'comment-form') {
+//
+//    // var wp = new WPAPI({
+//    //    endpoint: 'https://stumblingtowardsenlightenment.com/wp-json',
+//    //    username: 'bobo',
+//    //    password: 'mr?8(+JSx7z4'
+//    // });
