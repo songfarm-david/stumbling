96cea2ea612f8d7ee730721612f6010353d06ad9
David Gaskin <davidburkegaskin@gmail.com>
2019-06-26 13:14:34 -0700

    update sub created func

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,3 +1,67 @@
+const axios = require('axios')
+// require("dotenv").config()
+
+const {
+  MAILCHIMP_USERNAME,
+  MAILCHIMP_API_KEY,
+  MAILCHIMP_DATA_NO,
+  MAILCHIMP_LIST_ID
+} = process.env
+
+console.log('logging constants: \n',
+	MAILCHIMP_USERNAME,
+	MAILCHIMP_API_KEY,
+	MAILCHIMP_DATA_NO,
+	MAILCHIMP_LIST_ID
+);
+const TestAPIURL = 'https://us20.api.mailchimp.com/3.0/'
+const Credentials = MAILCHIMP_USERNAME+':'+ MAILCHIMP_API_KEY
+
+console.log('logging endpoint and credentials string: \n',
+	TestAPIURL,
+	Credentials
+);
+
+let testRequest = {
+   "email_address": "urist.mcvankab@freddurst.com",
+   "status": "subscribed",
+   "merge_fields": {
+      "FNAME": "Donnie",
+      "LNAME": "Brasco"
+   }
+}
+
+exports.handler = async (event, context, callback) => {
+	axios({
+		method: 'post',
+		url: TestAPIURL,
+		data: testRequest,
+		headers: {
+			'Authorization': 'Basic ' + Credentials
+		}
+	})
+	.then(res => {
+		console.log(res);
+	})
+	.catch(err => {
+		console.log(err);
+	})
+	.finally(() => {
+		console.log('Finally. Hello, world');
+	})
+}
+
+// Want to use async/await? Add the `async` keyword to your outer function/method.
+// async function getUser() {
+//   try {
+//     const response = await axios.get('/user?ID=12345');
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+
 //
 // const TestAPIURL = 'https://us20.api.mailchimp.com/3.0/lists/684872185b/members/'
 // // NOTE: these will be environmental variables
