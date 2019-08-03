bf7345082df97b65cbe0d75deb8eb49fd52344b5
David Gaskin <davidburkegaskin@gmail.com>
2019-08-02 22:54:54 -0700

    apply fix for oversize font awesome icon on page load

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
+import Helmet from "react-helmet"
 
+import { dom, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRss } from '@fortawesome/free-solid-svg-icons'
 
+// see: https://github.com/FortAwesome/react-fontawesome/issues/134
+config.autoAddCss = false
+
 const Nav = () => (
+	<>
+	<Helmet><style>{dom.css()}</style></Helmet>
 	<nav>
 		<div className="nav-section" id="topbar">
-			<Link to="/rss.xml" className="fa-icon rss-icon"><FontAwesomeIcon icon={faRss} /></Link>
+			<Link to="/rss.xml"><FontAwesomeIcon fixedWidth size="1x" icon={faRss} /></Link>
 		</div>
 		<div className="nav-section" id="links">
 			<Link to="/" className="nav-link">Home</Link>
@@ -16,6 +23,7 @@ const Nav = () => (
 
 		</div>
 	</nav>
+	</>
 )
 
 export default Nav
